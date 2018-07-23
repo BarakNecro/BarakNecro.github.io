0a3385e6c2422f0cc941d1d0267b4f5630acc394
SinZheng <31505014+BarakNecro@users.noreply.github.com>
2018-07-23 12:14:05 +0800

    1
    1

--- amWiki/mounts/icon-170.js
+++ amWiki/mounts/icon-170.js
@@ -1,1 +1,1 @@
-if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['icon']={"name":"icons.svg","path":"../amWiki/images/icons.svg","content":"","timestamp":1532317143865}
+if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['icon']={"name":"icons.svg","path":"../amWiki/images/icons.svg","content":"","timestamp":1532319216255}
